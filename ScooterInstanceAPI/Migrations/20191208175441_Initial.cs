24a245ee00704cd31ae789f1e2ec10fa6a590d4b
MosT98 <andreihalauca@yahoo.com>
2019-12-08 19:58:19 +0200

    first migrations

---
+++ ScooterInstanceAPI/Migrations/20191208175441_Initial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScooterInstanceAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scooter",
+                columns: table => new
+                {
+                    ScooterId = table.Column<Guid>(nullable: false),
+                    Brand = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    BatteryLife = table.Column<int>(nullable: false),
+                    AgeLimitMin = table.Column<int>(nullable: false),
+                    AgeLimitMax = table.Column<int>(nullable: false),
+                    WeightMax = table.Column<int>(nullable: false),
+                    SpeedLimit = table.Column<int>(nullable: false),
+                    ClimbAngle = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scooter", x => x.ScooterId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScooterInstances",
+                columns: table => new
+                {
+                    ScooterInstanceId = table.Column<Guid>(nullable: false),
+                    ScooterAutonomy = table.Column<int>(nullable: false),
+                    ScooterId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScooterInstances", x => x.ScooterInstanceId);
+                    table.ForeignKey(
+                        name: "FK_ScooterInstances_Scooter_ScooterId",
+                        column: x => x.ScooterId,
+                        principalTable: "Scooter",
+                        principalColumn: "ScooterId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Scooter",
+                columns: new[] { "ScooterId", "AgeLimitMax", "AgeLimitMin", "BatteryLife", "Brand", "ClimbAngle", "Model", "SpeedLimit", "WeightMax" },
+                values: new object[] { new Guid("74327683-ba31-4bef-bca7-e3b23212d24d"), 25, 15, 50, "Yamaha", 85, "v2", 500, 150 });
+
+            migrationBuilder.InsertData(
+                table: "ScooterInstances",
+                columns: new[] { "ScooterInstanceId", "ScooterAutonomy", "ScooterId" },
+                values: new object[] { new Guid("b3451766-f082-4981-9c44-add0d8d383c3"), 7, null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScooterInstances_ScooterId",
+                table: "ScooterInstances",
+                column: "ScooterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScooterInstances");
+
+            migrationBuilder.DropTable(
+                name: "Scooter");
+        }
+    }
+}
---
+++ UserAPI/Migrations/20191208175124_Initial.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<Guid>(nullable: false),
+                    Country = table.Column<string>(nullable: true),
+                    County = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    Number = table.Column<int>(nullable: false),
+                    Block = table.Column<string>(nullable: true),
+                    Floor = table.Column<int>(nullable: false),
+                    Apartment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    BirthDay = table.Column<DateTime>(nullable: false),
+                    SocialClass = table.Column<int>(nullable: false),
+                    AddressId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "AddressId", "Apartment", "Block", "City", "Country", "County", "Floor", "Number", "Street" },
+                values: new object[] { new Guid("ec2856fc-8c84-4330-9299-15ffba7b697a"), "7A", "Solomon", "Iasi", "Romania", "Iasi", 7, 35, "Moara de foc" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "AddressId", "BirthDay", "Email", "FirstName", "LastName", "Password", "Phone", "SocialClass" },
+                values: new object[] { new Guid("5e076492-8603-486c-a9df-1da58256938e"), null, new DateTime(1999, 12, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "catalinbelu21@gmail.com", "Catalin", "Belu", "parola", "0770796616", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AddressId",
+                table: "Users",
+                column: "AddressId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
