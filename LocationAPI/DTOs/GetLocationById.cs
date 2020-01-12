28738a9d319810f083d37d93c3dbe150b0f8fa41
MosT98 <andreihalauca@yahoo.com>
2020-01-13 01:28:43 +0200

    added method in location controller

---
+++ LocationAPI/DTOs/GetLocationById.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace LocationAPI.DTOs
+{
+    public class GetLocationById : IRequest<string>
+    {
+        public Guid LocationId { get; set; }
+        public GetLocationById(Guid locationId)
+        {
+            LocationId = locationId;
+        }
+    }
+}
