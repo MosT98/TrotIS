02483ad263fc8c79c97beb7ac568544db7cf0e7f
catalin99 <catalinbelu21@gmail.com>
2020-01-12 15:14:49 +0200

    scooter instances IDs

---
+++ LocationAPI/Business/GetScooterInstancesIDsHandler.cs
@@ -0,0 +1,28 @@
+﻿using LocationAPI.Data;
+using LocationAPI.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LocationAPI.Business
+{
+    public class GetScooterInstancesIDsHandler: IRequestHandler<GetScooterInstancesIDs, List<Guid>>
+    {
+        private readonly Context context;
+        public GetScooterInstancesIDsHandler(Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Guid>> Handle(GetScooterInstancesIDs request, CancellationToken cancellationToken)
+        {
+            var locations = await context.LinkLocationsToScooterInstances.ToListAsync();
+            var scooters =  locations.Where(a => a.LocationID == request.LocationID).Select(a => a.ScooterInstanceID).ToList();
+            return scooters;
+        }
+    }
+}
---
+++ LocationAPI/DTOs/GetScooterInstancesIDs.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationAPI.DTOs
+{
+    public class GetScooterInstancesIDs: IRequest<List<Guid>>
+    {
+        public Guid LocationID { get; set; }
+    }
+}
