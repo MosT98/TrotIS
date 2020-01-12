9907c9f83add96da54dfc6685f868d5c6a934237
MosT98 <andreihalauca@yahoo.com>
2020-01-12 18:27:49 +0200

    scooters get,post,delete

---
+++ ScooterInstanceAPI/DTOs/GetScooters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using ScooterInstanceAPI.Data;
+
+namespace ScooterInstanceAPI.DTOs
+{
+    public class GetScooters : IRequest<List<Scooter>>
+    {
+    }
+}
