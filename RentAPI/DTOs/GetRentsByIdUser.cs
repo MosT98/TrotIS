3831ac831ae091f54926b9e3381aa3f3e8a59569
MosT98 <andreihalauca@yahoo.com>
2020-01-13 02:26:14 +0200

    rent method added

---
+++ RentAPI/DTOs/GetRentsByIdUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+using RentAPI.Data;
+
+namespace RentAPI.DTOs
+{
+    public class GetRentsByIdUser : IRequest<List<Rent>>
+    {
+        public Guid UserId { get; set; }
+        public GetRentsByIdUser(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+}
