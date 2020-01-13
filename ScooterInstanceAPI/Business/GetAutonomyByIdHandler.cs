f27c79de37894cee66dfdf18c3ff728231d0ad03
MosT98 <andreihalauca@yahoo.com>
2020-01-13 02:02:17 +0200

    added method in scooterInstances

---
+++ ScooterInstanceAPI/Business/GetAutonomyByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ScooterInstanceAPI.Data;
+using ScooterInstanceAPI.DTOs;
+
+namespace ScooterInstanceAPI.Business
+{
+    public class GetAutonomyByIdHandler : IRequestHandler<GetAutonomyById, int>
+    {
+        private readonly Context context;
+
+        public GetAutonomyByIdHandler(Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task<int> Handle(GetAutonomyById request, CancellationToken cancellationToken)
+        {
+            var scooterInstance =
+                await context.ScooterInstances.SingleOrDefaultAsync(a => a.ScooterInstanceId == request.ScooterInstanceId);
+            return scooterInstance.ScooterAutonomy;
+        }
+    }
+}
